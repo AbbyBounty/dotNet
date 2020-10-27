64d04a933557daa7c4463991271a7cdc3ca2840c
abhilash <abhilash.kamble376@gmail.com>
2020-10-27 22:02:27 +0530

    Console.WriteLine ("this is new commit ");

--- codes/Day3/demo1/Program.cs
+++ codes/Day3/demo1/Program.cs
@@ -4,7 +4,7 @@ namespace demo1 {
 	class MainClass {
 		public static void Main (string [] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("this is new commit ");
 		}
 	}
 }
