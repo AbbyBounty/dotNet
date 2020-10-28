322cd1f4ac56c15d8d32beeba9ab350a1253ca15
abhilash <abhilash.kamble376@gmail.com>
2020-10-28 10:12:03 +0530

    Interface

---
+++ codes/Day3/LanguageFeatures/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace LanguageFeatures {
+
+
+
+	interface Iprintable {
+		void start ();
+
+		void print ();
+		void stop ();
+	}
+
+	class ThreeDPrinter : Iprintable {
+		public void start ()
+		{
+			Console.WriteLine ("printer started");
+
+		}
+
+
+		void Iprintable.print ()
+		{
+			Console.WriteLine ("printer printing..");
+		}
+
+
+		public void stop ()
+		{
+			Console.WriteLine ("printer stopped");
+
+		}
+	}
+
+	class MainClass {
+		public static void Main (string [] args)
+		{
+
+			int num = 12;
+			float price = 40.5f;
+			bool flag = false;
+			double score = 5600;
+			MainClass mainClass = new MainClass ();
+
+			Iprintable iprintable = null;
+
+
+			ThreeDPrinter threeDPrinter = new ThreeDPrinter ();
+			iprintable = threeDPrinter;
+			iprintable.start ();
+			iprintable.print ();
+			iprintable.stop ();
+
+
+
+		}
+	}
+}
